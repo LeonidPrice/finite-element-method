315b9a54ff3245d6a08a16631d0db9cfa4232c99
LeonidPrice <leonid.price.2.0@gmail.com>
2023-12-28 14:58:22 +0100

    Added canvas scaling and drawing primitives.

--- script.js
+++ script.js
@@ -1,5 +1,334 @@
+const scale_factor = 2;
+var scale = 1;
+var canvas_scale = 1;
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+const canvas_width = document.documentElement.scrollWidth;
+const canvas_height = document.documentElement.scrollHeight;
+const canvas_inner_width = canvas_width * scale_factor;
+const canvas_inner_height = canvas_height * scale_factor;
+const buffer = context.getImageData(0, 0, canvas.width, canvas.height);
+const step = buffer.width * 4;
+const dpi = 1/2.54;
+const ratio = context.miterLimit*dpi*scale_factor;
+canvas.width = canvas_inner_width;
+canvas.height = canvas_inner_height;
+canvas.style.width = canvas_width + 'px';
+canvas.style.height = canvas_height + 'px';
 
-var matrix = {
+const rad_deg = 180/Math.PI;
+const deg_rad = Math.PI/180;
+
+var position = {
+    x: 0, 
+    y: 0
+};
+
+var current_area = {
+    x0: 0, 
+    y0: 0,
+    x1: canvas_width,
+    y1: canvas_height
+};
+
+/**
+ * @class
+ * @param {Number} x Coordinate x.
+ * @param {Number} y Coordinate y.
+ */
+class NODE {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+/**
+ * @class
+ * @param {NODE} node_0 Start point of the line.
+ * @param {NODE} node_1 End point of the line.
+ */
+class LINE {
+    constructor(node_0, node_1) {
+        this.x0 = node_0.x;
+        this.y0 = node_0.y;
+        this.x1 = node_1.x;
+        this.y1 = node_1.y;
+    }
+}
+
+class ELEMENT {
+    constructor(node_1, node_2, fixing, E, A, n) {
+        const line = new LINE(node_1, node_2);
+        this.x0 = line.x0;
+        this.y0 = line.y0;
+        this.x1 = line.x1;
+        this.y1 = line.y1;
+        this.fixing = fixing;
+        this.E = E;
+        this.A = A;
+        this.n = n;
+    }
+
+    length() {
+        return Math.sqrt((this.x1 - this.x0) ** 2 + (this.y1 - this.y0) ** 2);
+    }
+
+    angle() {
+        return Math.atan((this.y1 - this.y0) / (this.x1 - this.x0)) * rad_deg;
+    }
+
+    cos() {
+        return (this.x1 - this.x0) / this.length();
+    }
+
+    sin() {
+        return (this.y1 - this.y0) / this.length();
+    }
+}
+
+var DRAW = {
+    /**
+     * Takes into account the constancy of line thickness during scaling
+     * @param {Boolean} zooming True - the line is scaled, false - the line has a constant size.
+     * @returns {Number} Scale factor.
+     */
+    multiplier: function(zooming) {
+        if (zooming === true) {
+            return ratio;
+        } else {
+            return ratio/scale;
+        }
+    },
+
+    /**
+     * Represents lines in accordance with GOST 2.303-68 (government standard).
+     */
+    line: {
+        /**
+         * @param {LINE} line An instance of the LINE class representing the start and end points of a line.
+         * @param {Number[]} color Default - black.
+         * @param {Number} width Default - 1mm.
+         * @param {Boolean} zooming - Scale factor, default true.
+         */
+        main: function(line, color=[0,0,0,255], width = 1, zooming = true) {
+            context.beginPath();
+            context.lineWidth = width*DRAW.multiplier(zooming);
+            context.lineJoin = 'round';
+            context.lineCap = 'round';
+            context.strokeStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+
+            context.moveTo((line.x0*ratio - current_area.x0), (line.y0*ratio - current_area.y0));
+            context.lineTo((line.x1*ratio - current_area.x0), (line.y1*ratio - current_area.y0));
+            context.stroke();
+            context.closePath();
+        },
+
+        /**
+         * @param {LINE} line An instance of the LINE class representing the start and end points of a line.
+         * @param {Number[]} color Default - black.
+         * @param {Number} width Default - 0,5mm.
+         * @param {Boolean} zooming - Scale factor, default true.
+         */
+        thin: function(line, color=[0,0,0,255], width = 0.5, zooming = true) {
+            context.beginPath();
+            context.lineWidth = width*DRAW.multiplier(zooming);
+            context.lineJoin = 'round';
+            context.lineCap = 'round';
+            context.strokeStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+            context.moveTo((canvas_inner_width/2 + line.x0*ratio), (canvas_inner_height/2 - line.y0*ratio));
+            context.lineTo((canvas_inner_width/2 + line.x1*ratio), (canvas_inner_height/2 - line.y1*ratio));
+            context.stroke();
+            context.closePath();
+        },
+
+        /**
+         * @param {LINE} line An instance of the LINE class representing the start and end points of a line.
+         * @param {Number[]} color Default - black.
+         * @param {Number} width Default - 0,5mm.
+         * @param {Boolean} zooming - Scale factor, default true.
+         */
+        dashed: function(line, color=[0,0,0,255], width = 0.5, zooming = true) {
+            context.beginPath();
+            context.lineWidth = width*DRAW.multiplier(zooming);
+            context.lineJoin = 'round';
+            context.lineCap = 'round';
+            context.strokeStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+            context.moveTo((canvas_inner_width/2 + line.x0*ratio), (canvas_inner_height/2 - line.y0*ratio));
+            context.lineTo((canvas_inner_width/2 + line.x1*ratio), (canvas_inner_height/2 - line.y1*ratio));
+            context.setLineDash([2*ratio, 2*ratio]);
+            context.stroke();
+            context.closePath();
+        },
+    },
+
+    /**
+     * @param {LINE} line An instance of the LINE class representing the start and end points of a line.
+     * @param {Number[]} color Default - black.
+     * @param {Number} width Default - 0,5mm.
+     * @param {Number} arrow_length The default is 2.5mm according to the standard. 
+     * @param {Boolean} zooming - Scale factor, default false.
+     */
+    arrow: function(line, color = [0, 0, 0, 255], width = 0.25, arrow_length = 5, zooming = false) {
+        var x0 = line.x0;
+        var y0 = line.y0;
+        var x1 = line.x1;
+        var y1 = line.y1;
+        var length = ((x1-x0)**2+(y1-y0)**2)**(1/2);
+        var tan_alpha = (y1 - y0) / (x1 - x0);
+        var cos_alpha = (x1-x0) / length;
+        var sin_alpha = (y1-y0) / length;
+        var tan_beta = -1 / tan_alpha;
+        var beta = Math.atan(tan_beta) * rad_deg;
+        var a = arrow_length * Math.tan(10 * deg_rad);
+        var base = [
+            (length-arrow_length)*cos_alpha + x0,
+            (length-arrow_length)*sin_alpha + y0
+        ];
+        var points = [
+            base[0] + a * Math.cos(beta * deg_rad),
+            base[1] + a * Math.sin(beta * deg_rad),
+            base[0] - a * Math.cos(beta * deg_rad),
+            base[1] - a * Math.sin(beta * deg_rad)
+        ];
+
+        context.beginPath();
+        context.lineWidth = width*DRAW.multiplier(zooming);
+        context.lineJoin = 'round';
+        context.lineCap = 'round';
+        context.strokeStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+        context.moveTo((canvas_inner_width / 2 + x0 * ratio), (canvas_inner_height / 2 - y0 * ratio));
+        context.lineTo((canvas_inner_width / 2 + x1 * ratio), (canvas_inner_height / 2 - y1 * ratio));
+        context.stroke();
+        context.closePath();
+
+        context.beginPath();
+        context.lineWidth = width*DRAW.multiplier(zooming);
+        context.lineJoin = 'miter';
+        context.lineCap = 'round';
+        context.strokeStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+        context.fillStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+        context.moveTo((canvas_inner_width / 2 + points[0] * ratio), (canvas_inner_height / 2 - points[1] * ratio));
+        context.lineTo((canvas_inner_width / 2 + x1 * ratio), (canvas_inner_height / 2 - y1 * ratio));
+        context.lineTo((canvas_inner_width / 2 + points[2] * ratio), (canvas_inner_height / 2 - points[3] * ratio));
+        context.closePath();
+        context.stroke();
+        context.fill();
+    },
+
+    /**
+     * 
+     * @param {NODE} node An instance of the NODE class representing coordinates of the piont.
+     * @param {Number} size Length of the arrows.
+     */
+    coordinate_system: function(node, size) {
+        DRAW.arrow(new LINE(new NODE(node.x,node.y), new NODE(node.x+size,node.y)), [0,255,0,255], 0.25, 2.5, false)
+        DRAW.arrow(new LINE(new NODE(node.x,node.y), new NODE(node.x,node.y+size)), [255,0,0,255], 0.25, 2.5, false)
+    },
+
+    circle: {
+        /** 
+         * @param {NODE} node An instance of the NODE class representing coordinates of the piont.
+         * @param {Number} diameter Size of the diameter in mm.
+         * @param {Number[]} color Default - black.
+         * @param {Number} width Default 0.25mm.
+         * @param {Boolean} zooming - Scale factor, default true.
+         */
+        fill: function(node, diameter, color = [0, 0, 0, 255], width = 0.25, zooming = true) {
+            context.beginPath();
+            context.lineWidth = width*DRAW.multiplier(zooming);
+            context.fillStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+            context.arc((canvas_inner_width/2 + node.x*ratio), (canvas_inner_height/2 - node.y*ratio), (diameter/2)*DRAW.multiplier(zooming), 0, 2*Math.PI);
+            context.fill();
+            context.closePath();
+        },
+
+        /** 
+         * @param {NODE} node An instance of the NODE class representing coordinates of the piont.
+         * @param {Number} diameter Size of the diameter in mm.
+         * @param {Number[]} color Default - black.
+         * @param {Number} width Default 0.25mm.
+         * @param {Boolean} zooming - Scale factor, default true.
+         */
+        stroke: function(node, diameter, color = [0, 0, 0, 255], width = 0.25, zooming = true) {
+            context.beginPath();
+            context.lineWidth = width*DRAW.multiplier(zooming);
+            context.strokeStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+            context.arc((canvas_inner_width/2 + node.x*ratio), (canvas_inner_height/2 - node.y*ratio), (diameter/2)*DRAW.multiplier(zooming), 0, 2*Math.PI);
+            context.stroke();
+            context.closePath();
+        },
+    },
+}
+
+var CANVAS = {
+    update: function() {
+        context.putImageData(buffer, 0, 0);
+    },
+
+    /**
+     * Represents all elelements in the canvas.
+     */
+    draw: function() {
+        if (context === null) {return;}
+
+        DRAW.coordinate_system(new NODE(0,0), 20)
+        DRAW.circle.fill(new NODE(0,0),1,[0,0,0,255],false);
+        DRAW.circle.stroke(new NODE(0,0),2,[0,0,0,255]);
+    },
+
+    /**
+     * Scales the canvas when the mouse wheel is scrolled relative to the current cursor position.
+     */
+    scale: function() {
+        canvas.addEventListener("wheel", onmousewheel, false);
+        canvas.addEventListener("DOMMouseScroll", onmousewheel, false);
+    
+        function onmousewheel(event) {
+            var mouse_x = event.clientX*2;
+            var mouse_y = event.clientY*2;
+            var delta = event.type === "wheel" ? event.wheelDelta : -event.detail;
+            var increment = delta > 0 ? 1.1 : 0.9;
+            scale *= increment;
+
+            position.x = mouse_x;
+            position.y = mouse_y;
+
+            current_area.x0 -= mouse_x;
+            current_area.y0 -= mouse_y;
+            current_area.x1 -= mouse_x;
+            current_area.y1 -= mouse_y;
+            current_area.x0 *= increment;
+            current_area.y0 *= increment;
+            current_area.x1 *= increment;
+            current_area.y1 *= increment;
+            current_area.x0 += mouse_x;
+            current_area.y0 += mouse_y;
+            current_area.x1 += mouse_x;
+            current_area.y1 += mouse_y;
+
+            function redraw() {
+                canvas_scale = -(current_area.x0 - current_area.x1) / canvas_width;
+                context.setTransform(1, 0, 0, 1, 0, 0);
+                context.clearRect(0, 0, canvas_inner_width, canvas_inner_height);
+                context.setTransform(canvas_scale, 0, 0, canvas_scale, current_area.x0, current_area.y0);
+                CANVAS.draw();
+                requestAnimationFrame(() => redraw());
+            }
+            redraw();
+            event.preventDefault();
+        }
+    },
+}
+CANVAS.draw();
+CANVAS.scale();
+
+document.addEventListener("mousemove", event => {
+    position.x = event.clientX*2;
+    position.y = event.clientY*2;
+})
+
+var MATRIX = {
     errors : [
         "MATRIX ERROR: The matrices aren't compatible.\nThe number of columns of matrix A must be equal to the number of rows of matrix B.",
         "MATRIX ERROR: The matrices aren't compatible.\nThe number of rows and columns of matrices A and B must be equal.",
@@ -141,7 +470,7 @@ var matrix = {
                 first_norma.push(s)
                 s = 0;
             }
-            return matrix.selection_sort(first_norma)[0][0];
+            return MATRIX.selection_sort(first_norma)[0][0];
         },
 
         /**
@@ -165,7 +494,7 @@ var matrix = {
                 infinity_norma.push(s)
                 s = 0;
             }
-            return matrix.selection_sort(infinity_norma)[0][0];
+            return MATRIX.selection_sort(infinity_norma)[0][0];
         }
     },
 
@@ -366,7 +695,7 @@ var matrix = {
         for (var i = 0; i < A[0].length; i++) {
             A_T[i] = [];
             for (var j = 0; j < A.length; j++) {
-                A_T[j][i] = A[i][j]
+                A_T[i][j] = A[j][i];
             }
         }
 
